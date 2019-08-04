7ca585e036520bb4139583ef94cfad6596db08da
Renato Hermoza <renato145@hotmail.com>
2019-08-05 00:32:34 +0930

    update

--- nbs/maryam/run_parallel.py
+++ nbs/maryam/run_parallel.py
@@ -3,12 +3,13 @@ from parallel_ex import ParallelEx
 
 def run():
     kwargs = [
-        dict(experiment='exp1', method='noNNReval', runs=4, frequency=100, pbar=False),
-        dict(experiment='exp2', method='noNNReval', runs=4, frequency=100, pbar=False),
-        dict(experiment='exp3', method='noNNReval', runs=4, frequency=100, pbar=False),
-        dict(experiment='exp4', method='noNNReval', runs=4, frequency=100, pbar=False),
+        dict(experiment='exp1', method='noNNReval', runs=6, frequency=10, pbar=False),
+        dict(experiment='exp2', method='noNNReval', runs=6, frequency=10, pbar=False),
+        dict(experiment='exp2', method='noNNReval', runs=6, frequency=10, pbar=False),
+        dict(experiment='exp4', method='noNNReval', runs=6, frequency=10, pbar=False),
+        dict(experiment='exp4', method='noNNReval', runs=6, frequency=10, pbar=False),
     ]
-    ParallelEx.run_kwargs(main, kwargs, totals=4, n_cpu=2)
+    ParallelEx.run_kwargs(main, kwargs, totals=6, n_cpu=2)
 
 if __name__ == "__main__":
     run()
