5c7fa64395f214116663e1e56e738942e83467d8
Maryam Hasani <maryam@Maryas-MacBook-Pro.local>
2019-08-05 13:56:14 +0930

    update experiments

--- nbs/maryam/run_experiment.sh
+++ nbs/maryam/run_experiment.sh
@@ -1,1 +1,24 @@
-~/anaconda3/envs/DENN/bin/python run_experiment.py exp1 NNnorm Worst
+#Experiment = Enum('Experiment', 'exp1 exp2 exp3 exp4')
+#Method = Enum('Methods', 'noNNRestart noNNReval NNnorm NNdrop')
+#Mechname:Random, Worst, Closest
+
+# ~/anaconda3/envs/DENN/bin/python run_experiment.py exp1 NNnorm Worst --D 30 --runs 30 --frequency 1000 --max_times 100 --nn_window 5 --nn_nf 4 --nn_pick 3
+
+#exp1
+~/anaconda3/envs/DENN/bin/python run_experiment.py exp1 noNNReval  --runs 30 --frequency 1200
+~/anaconda3/envs/DENN/bin/python run_experiment.py exp1 noNNRestart  --runs 30 --frequency 1200
+~/anaconda3/envs/DENN/bin/python run_experiment.py exp1 NNnorm  Worst --runs 30 --frequency 1000
+~/anaconda3/envs/DENN/bin/python run_experiment.py exp1 NNdrop  Worst --runs 30 --frequency 1000
+~/anaconda3/envs/DENN/bin/python run_experiment.py exp1 NNnorm  Closest --runs 30 --frequency 1000
+~/anaconda3/envs/DENN/bin/python run_experiment.py exp1 NNdrop  Closest --runs 30 --frequency 1000
+~/anaconda3/envs/DENN/bin/python run_experiment.py exp1 NNnorm  Random --runs 30 --frequency 1000
+~/anaconda3/envs/DENN/bin/python run_experiment.py exp1 NNdrop  Random --runs 30 --frequency 1000
+
+#exp4
+#~/anaconda3/envs/DENN/bin/python run_experiment.py exp4 noNNReval  --runs 30 --frequency 1150
+#~/anaconda3/envs/DENN/bin/python run_experiment.py exp4 noNNRestart  --runs 30 --frequency 1150
+#~/anaconda3/envs/DENN/bin/python run_experiment.py exp4 NNnorm  Worst --runs 30 --frequency 1000
+#~/anaconda3/envs/DENN/bin/python run_experiment.py exp4 NNdrop  Worst --runs 30 --frequency 1000
+#~/anaconda3/envs/DENN/bin/python run_experiment.py exp4 NNnorm  Closest --runs 30 --frequency 1000
+#~/anaconda3/envs/DENN/bin/python run_experiment.py exp4 NNdrop  Closest --runs 30 --frequency 1000
+#~/anaconda3/envs/DENN/bin/python run_experiment.py exp4 NNnorm  Random --runs 30 --frequency 1000
