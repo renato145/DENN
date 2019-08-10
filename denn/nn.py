75e155a1ad863a48726a5b4b2a03296160c62882
Maryam Hasani <maryam@Maryas-MacBook-Pro.local>
2019-08-10 18:16:08 +0930

    add sample_size param

--- denn/nn.py
+++ denn/nn.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn, functional as F, Tensor
+from itertools import product
 from .imports import *
 from .callbacks import *
 from .optimization import *
@@ -21,17 +22,34 @@ class NNTrainer(Callback):
     _order = 10 # Needs to run after restarting the population 
 
     def __init__(self, optim:'Optimization', model:nn.Module, replace_mechanism:ReplaceMechanism=ReplaceMechanism.Random, n:int=3, noise_range:float=0.5,
-                 window:int=5, min_batches:int=20, bs:int=4, epochs:int=10, loss_func:Callable=nn.MSELoss(), nn_optim:torch.optim.Optimizer=torch.optim.Adam):
-        'TODO: add documentation'
+                 sample_size:int=1, window:int=5, min_batches:int=20, bs:int=4, epochs:int=10, loss_func:Callable=nn.MSELoss(),
+                 nn_optim:torch.optim.Optimizer=torch.optim.Adam):
+        '''Uses neural network to initialize individuals after a change is detected.
+        Params:
+          - optim: Optimization class.
+          - model: Neural network model.
+          - replace_mechanism: ReplaceMechanism.
+          - n: Number of individuals to predict and replace.
+          - noise_range: Noise to add to the predictions.
+          - sample_size: The number of individuals to take at each time change.
+          - window: Number of past individuals to predict the next one.
+          - min_batches: Minimun amount of batches to start training.
+          - bs: Batch size for the neural network.
+          - epochs: Number of epochs to train the neural network at each time change.
+          - loss_func: Loss function.
+          - nn_optim : Optimizer for the neural network.
+          '''
         super().__init__(optim)
-        self.model,self.replace_mechanism,self.n,self.noise_range,self.window,self.min_batches,self.bs,self.epochs,self.loss_func =\
-             model,     replace_mechanism,     n,     noise_range,     window,     min_batches,     bs,     epochs,     loss_func
+        self.model,self.replace_mechanism,self.n,self.noise_range,self.sample_size,self.window,self.min_batches,self.bs,self.epochs,self.loss_func =\
+             model,     replace_mechanism,     n,     noise_range,     sample_size,     window,     min_batches,     bs,     epochs,     loss_func
         self.data,self.train_losses = [],[]
         self.nn_optim = nn_optim(model.parameters())
         self.d = optim.population.dimension
         self.n_individuals = optim.population.n
         self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
         self.model.to(self.device)
+        self.batch_per_time = self.sample_size**self.window
+        self.data_x,self.data_y = [],[]
         optim.callbacks.append(NNTimer)
         optim.metrics.append(NNTimer)
 
@@ -57,11 +75,13 @@ class NNTrainer(Callback):
         elif self.replace_mechanism==ReplaceMechanism.Closest: self._replace_closest(preds)
         elif self.replace_mechanism==ReplaceMechanism.Worst  : self._replace_worst  (preds)
 
-    def on_detect_change_end(self, change_detected:bool, best:Individual, **kwargs:Any)->None:
+    def on_detect_change_end(self, change_detected:bool, **kwargs:Any)->None:
         start_time = get_time()
         if change_detected:
-            self.data.append(best.clone())
-            if len(self.data)-self.window >= self.min_batches:
+            this_bests = [indiv.clone() for indiv in self.optim.population.get_n_best(self.sample_size)]
+            self.data.append(this_bests)
+            if (len(self.data)-self.window-1) > 0: self.update_data()
+            if self.batch_per_time*(len(self.data)-self.window-1) >= self.min_batches:
                 self.do_train()
                 self.apply_predictions()
 
@@ -70,15 +90,21 @@ class NNTrainer(Callback):
     def on_run_end(self, **kwargs:Any)->None:
         self.model.eval()
 
-    def get_train_data(self)->Tuple[Tensor,Tensor]:
-        w,d = self.window,self.d
-        data = torch.from_numpy(np.vstack([e.data for e in self.data])).float()
-        data_x,data_y = [],[]
-        for i in range(data.size(0)-w):
-            data_x.append(data[i:i+w])
-            data_y.append(data[i+w])
+    def update_data(self)->None:
+        w = self.window
+        data_x = []
+        this_x = self.data[-(w+1):-1]
+        for idxs in product(range(self.sample_size), repeat=w):
+            tx = torch.from_numpy(np.vstack([x[idx].data for idx,x in zip(idxs,this_x)])).float()
+            data_x.append(tx)
+            
+        data_x = torch.stack(data_x).float()
+        data_y = torch.from_numpy(self.data[-1][0].data).float() # The best individual of the last time
+        self.data_x.append(data_x)
+        self.data_y.append(data_y.repeat(data_x.size(0),1))
 
-        return torch.stack(data_x).to(self.device),torch.stack(data_y).to(self.device)
+    def get_train_data(self)->Tuple[Tensor,Tensor]:
+        return torch.cat(self.data_x).to(self.device),torch.cat(self.data_y).to(self.device)
 
     def do_train(self)->None:
         bs,epochs,model,loss_func,nn_optim = self.bs,self.epochs,self.model,self.loss_func,self.nn_optim
