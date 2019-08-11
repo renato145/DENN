4526ebc4a7fb6563e5ca2af4e65685927d4cc0d3
Maryam Hasani <maryam@Maryas-MacBook-Pro.local>
2019-08-11 20:10:18 +0930

    changes of replacement of pop with predicted methods

--- nbs/maryam/run_parallel.py
+++ nbs/maryam/run_parallel.py
@@ -3,14 +3,20 @@ from parallel_ex import ParallelEx
 
 def run():
     kwargs = [
-        dict(experiment='exp4', func_name='sphere', method='noNNReval', runs=20, frequency=1200, pbar=False),
-        #dict(experiment='exp1', func_name='rastrigin', method='noNNRestart', runs=30, frequency=1200, pbar=False),
-        dict(experiment='exp4', func_name='sphere', method='NNnorm', replace_mech='Worst', runs=20, frequency=1000, pbar=False),
-        dict(experiment='exp4', func_name='sphere', method='NNdrop', replace_mech='Worst', runs=20, frequency=1000, pbar=False),
-        dict(experiment='exp4', func_name='sphere', method='NNnorm', replace_mech='Closest', runs=20, frequency=1000, pbar=False),
-        dict(experiment='exp4', func_name='sphere', method='NNdrop', replace_mech='Closest', runs=20, frequency=1000, pbar=False),
-        dict(experiment='exp4', func_name='sphere', method='NNnorm', replace_mech='Random', runs=20, frequency=1000, pbar=False),
-        dict(experiment='exp4', func_name='sphere', method='NNdrop', replace_mech='Random', runs=20, frequency=1000, pbar=False),
+        dict(experiment='exp2', func_name='sphere', method='NNnorm', replace_mech='Random', runs=1, frequency=1000,
+             nn_sample_size=1, pbar=False),
+        dict(experiment='exp2', func_name='sphere', method='NNnorm', replace_mech='Random', runs=1, frequency=1000,
+             nn_sample_size=3, pbar=False),
+        dict(experiment='exp2', func_name='sphere', method='NNnorm', replace_mech='Random', runs=1, frequency=1000,
+             nn_sample_size=5, pbar=False),
+        # dict(experiment='exp4', func_name='sphere', method='noNNReval', runs=20, frequency=1200, pbar=False),
+        # dict(experiment='exp1', func_name='rastrigin', method='noNNRestart', runs=30, frequency=1200, pbar=False),
+        # dict(experiment='exp4', func_name='sphere', method='NNnorm', replace_mech='Worst', runs=20, frequency=1000, pbar=False),
+        # dict(experiment='exp4', func_name='sphere', method='NNdrop', replace_mech='Worst', runs=20, frequency=1000, pbar=False),
+        # dict(experiment='exp4', func_name='sphere', method='NNnorm', replace_mech='Closest', runs=20, frequency=1000, pbar=False),
+        # dict(experiment='exp4', func_name='sphere', method='NNdrop', replace_mech='Closest', runs=20, frequency=1000, pbar=False),
+        # dict(experiment='exp4', func_name='sphere', method='NNnorm', replace_mech='Random', runs=20, frequency=1000, pbar=False),
+        # dict(experiment='exp4', func_name='sphere', method='NNdrop', replace_mech='Random', runs=20, frequency=1000, pbar=False),
     ]
     ParallelEx.run_kwargs(main, kwargs, totals=30, n_cpu=None)
 
