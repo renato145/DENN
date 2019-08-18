2bb1f717d8e25af28332534b01d7174c5fd70bab
Maryam Hasani <maryam@Maryas-MacBook-Pro.local>
2019-08-18 16:04:58 +0930

    updates

--- nbs/maryam/run_parallel.py
+++ nbs/maryam/run_parallel.py
@@ -8,14 +8,14 @@ def run():
           #  nn_sample_size=3, pbar=False),
        # dict(experiment='exp2', func_name='sphere', method='NNnorm', replace_mech='Random', runs=1, frequency=1000, nn_train_window=5,
         #    nn_sample_size=5, pbar=False),
-         dict(experiment='exp2', func_name='sphere', method='noNNReval', runs=20, frequency=1150, pbar=False),
-        # dict(experiment='exp1', func_name='sphere', method='noNNRestart', runs=30, frequency=1200, pbar=False),
-         dict(experiment='exp2', func_name='sphere', method='NNnorm', replace_mech='Worst', runs=20, frequency=1000, pbar=False),
-         dict(experiment='exp2', func_name='sphere', method='NNdrop', replace_mech='Worst', runs=20, frequency=1000, pbar=False),
-         dict(experiment='exp2', func_name='sphere', method='NNnorm', replace_mech='Closest', runs=20, frequency=1000, pbar=False),
-         dict(experiment='exp2', func_name='sphere', method='NNdrop', replace_mech='Closest', runs=20, frequency=1000, pbar=False),
-         dict(experiment='exp2', func_name='sphere', method='NNnorm', replace_mech='Random', runs=20, frequency=1000, pbar=False),
-         dict(experiment='exp2', func_name='sphere', method='NNdrop', replace_mech='Random', runs=20, frequency=1000, pbar=False),
+         dict(experiment='exp2', func_name='rosenbrock', method='noNNReval', runs=20, frequency=1150, pbar=False),
+        # dict(experiment='exp1', func_name='rosenbrock', method='noNNRestart', runs=30, frequency=1200, pbar=False),
+         dict(experiment='exp2', func_name='rosenbrock', method='NNnorm', replace_mech='Worst', runs=20, frequency=1000, pbar=False),
+         dict(experiment='exp2', func_name='rosenbrock', method='NNdrop', replace_mech='Worst', runs=20, frequency=1000, pbar=False),
+       #  dict(experiment='exp2', func_name='sphere', method='NNnorm', replace_mech='Closest', runs=20, frequency=1000, pbar=False),
+       #  dict(experiment='exp2', func_name='sphere', method='NNdrop', replace_mech='Closest', runs=20, frequency=1000, pbar=False),
+       #  dict(experiment='exp2', func_name='sphere', method='NNnorm', replace_mech='Random', runs=20, frequency=1000, pbar=False),
+       #  dict(experiment='exp2', func_name='sphere', method='NNdrop', replace_mech='Random', runs=20, frequency=1000, pbar=False),
     ]
     ParallelEx.run_kwargs(main, kwargs, totals=30, n_cpu=None)
 
