31fc3c49409fa0eb9f60b73475d88351742c992f
Maryam Hasani Shoreh <a1711759@l03.phoenix.adelaide.edu.au>
2019-11-08 10:21:09 +1030

    reapeat exp3 nnpick and sampleSize

--- ClusterFiles/run_cluster_experiments.bash
+++ ClusterFiles/run_cluster_experiments.bash
@@ -158,17 +158,17 @@
 
 # sbatch --export=ALL,experiment="exp1",func_name="rosenbrock",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=5,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
 # sbatch --export=ALL,experiment="exp2",func_name="rosenbrock",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=5,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
-# sbatch --export=ALL,experiment="exp3",func_name="rosenbrock",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=5,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+#sbatch --export=ALL,experiment="exp3",func_name="rosenbrock",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=5,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
 # sbatch --export=ALL,experiment="exp4",func_name="rosenbrock",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=5,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
 
 # sbatch --export=ALL,experiment="exp1",func_name="sphere",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=5,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
 # sbatch --export=ALL,experiment="exp2",func_name="sphere",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=5,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
-# sbatch --export=ALL,experiment="exp3",func_name="sphere",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=5,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+#sbatch --export=ALL,experiment="exp3",func_name="sphere",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=5,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
 # sbatch --export=ALL,experiment="exp4",func_name="sphere",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=5,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
 
 # sbatch --export=ALL,experiment="exp1",func_name="rastrigin",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=5,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
 # sbatch --export=ALL,experiment="exp2",func_name="rastrigin",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=5,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
-# sbatch --export=ALL,experiment="exp3",func_name="rastrigin",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=5,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+#sbatch --export=ALL,experiment="exp3",func_name="rastrigin",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=5,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
 # sbatch --export=ALL,experiment="exp4",func_name="rastrigin",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=5,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
 
 
@@ -188,32 +188,34 @@
 # # sbatch --export=ALL,experiment="exp3",func_name="rastrigin",method="noNNReval",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=2,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
 # # sbatch --export=ALL,experiment="exp4",func_name="rastrigin",method="noNNReval",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=2,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
 
+#reapeat sample size exp for exp 3
+
+# sbatch --export=ALL,experiment="exp3",func_name="rosenbrock",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=1,save="True",pbar="False",silent="False",cluster="True",nn_train_window=None,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+# sbatch --export=ALL,experiment="exp3",func_name="sphere",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=1,save="True",pbar="False",silent="False",cluster="True",nn_train_window=None,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+# sbatch --export=ALL,experiment="exp3",func_name="rastrigin",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=1,save="True",pbar="False",silent="False",cluster="True",nn_train_window=None,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+
+#reapeat nnpick exp for exp 3
+sbatch --export=ALL,experiment="exp3",func_name="rosenbrock",method="NNnorm",replace_mech="Worst",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=7,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+sbatch --export=ALL,experiment="exp3",func_name="sphere",method="NNnorm",replace_mech="Worst",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=7,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+sbatch --export=ALL,experiment="exp3",func_name="rastrigin",method="NNnorm",replace_mech="Worst",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=7,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+
+sbatch --export=ALL,experiment="exp3",func_name="rosenbrock",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=7,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+sbatch --export=ALL,experiment="exp3",func_name="sphere",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=7,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+sbatch --export=ALL,experiment="exp3",func_name="rastrigin",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=7,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+
+sbatch --export=ALL,experiment="exp3",func_name="rosenbrock",method="NNnorm",replace_mech="Worst",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=9,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+sbatch --export=ALL,experiment="exp3",func_name="sphere",method="NNnorm",replace_mech="Worst",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=9,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+sbatch --export=ALL,experiment="exp3",func_name="rastrigin",method="NNnorm",replace_mech="Worst",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=9,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+
+sbatch --export=ALL,experiment="exp3",func_name="rosenbrock",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=9,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+sbatch --export=ALL,experiment="exp3",func_name="sphere",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=9,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+sbatch --export=ALL,experiment="exp3",func_name="rastrigin",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=9,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
 
 
-##reapeat exp3
+sbatch --export=ALL,experiment="exp3",func_name="rosenbrock",method="NNnorm",replace_mech="Worst",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=1,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+sbatch --export=ALL,experiment="exp3",func_name="sphere",method="NNnorm",replace_mech="Worst",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=1,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+sbatch --export=ALL,experiment="exp3",func_name="rastrigin",method="NNnorm",replace_mech="Worst",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=1,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
 
-#freq=0.5
-sbatch --export=ALL,experiment="exp3",func_name="rosenbrock",method="NNnorm",replace_mech="Worst",D=30,runs=30,frequency=0.5,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=0.5,batch_size=4,nn_epochs=3 cluster_job.sh
-
-sbatch --export=ALL,experiment="exp3",func_name="sphere",method="NNnorm",replace_mech="Worst",D=30,runs=30,frequency=0.5,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=0.5,batch_size=4,nn_epochs=3 cluster_job.sh
-
-sbatch --export=ALL,experiment="exp3",func_name="rastrigin",method="NNnorm",replace_mech="Worst",D=30,runs=30,frequency=0.5,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=0.5,batch_size=4,nn_epochs=3 cluster_job.sh
-
-sbatch --export=ALL,experiment="exp3",func_name="rosenbrock",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=0.5,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=0.5,batch_size=4,nn_epochs=3 cluster_job.sh
-
-sbatch --export=ALL,experiment="exp3",func_name="sphere",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=0.5,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=0.5,batch_size=4,nn_epochs=3 cluster_job.sh
-
-sbatch --export=ALL,experiment="exp3",func_name="rastrigin",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=0.5,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=0.5,batch_size=4,nn_epochs=3 cluster_job.sh
-
-#freq=4
-sbatch --export=ALL,experiment="exp3",func_name="rosenbrock",method="NNnorm",replace_mech="Worst",D=30,runs=30,frequency=4,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=4,batch_size=4,nn_epochs=3 cluster_job.sh
-
-sbatch --export=ALL,experiment="exp3",func_name="sphere",method="NNnorm",replace_mech="Worst",D=30,runs=30,frequency=4,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=4,batch_size=4,nn_epochs=3 cluster_job.sh
-
-sbatch --export=ALL,experiment="exp3",func_name="rastrigin",method="NNnorm",replace_mech="Worst",D=30,runs=30,frequency=4,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=4,batch_size=4,nn_epochs=3 cluster_job.sh
-
-sbatch --export=ALL,experiment="exp3",func_name="rosenbrock",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=4,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=4,batch_size=4,nn_epochs=3 cluster_job.sh
-
-sbatch --export=ALL,experiment="exp3",func_name="sphere",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=4,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=4,batch_size=4,nn_epochs=3 cluster_job.sh
-
-sbatch --export=ALL,experiment="exp3",func_name="rastrigin",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=4,max_times=100,nn_window=5,nn_nf=4,nn_pick=3,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=4,batch_size=4,nn_epochs=3 cluster_job.sh
+sbatch --export=ALL,experiment="exp3",func_name="rosenbrock",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=1,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+sbatch --export=ALL,experiment="exp3",func_name="sphere",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=1,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
+sbatch --export=ALL,experiment="exp3",func_name="rastrigin",method="NNnorm",replace_mech="Random",D=30,runs=30,frequency=1,max_times=100,nn_window=5,nn_nf=4,nn_pick=1,nn_sample_size=3,save="True",pbar="False",silent="False",cluster="True",nn_train_window=5,freqSave=1,batch_size=4,nn_epochs=3 cluster_job.sh
