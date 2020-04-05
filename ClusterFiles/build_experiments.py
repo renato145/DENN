2c290cac4a8091bd6acf977e8510339e59f82272
Maryam Hasani Shoreh <a1711759@l03.phoenix.adelaide.edu.au>
2020-04-05 15:56:29 +0930

    convolution exps

--- ClusterFiles/build_experiments.py
+++ ClusterFiles/build_experiments.py
@@ -13,12 +13,12 @@ lines = []
 # for replace_mech in ['Random','Worst']:
 	# for sample_size in [1,3,7,9]: noNNRestart
 for freq in [0.5,1,4,10,20]:	
-	for diversity_method in ['None']:#'RI', 'None', 'CwN', 'HMu'
+	for diversity_method in ['RI', 'None', 'CwN', 'HMu','Rst']:#'RI', 'None', 'CwN', 'HMu'
 		for exp in ['exp1','exp2','exp3','exp4']:
 			for func in ['sphere','rosenbrock','rastrigin']:
 				# train_window = None if sample_size==1 else 5
 				# epochs       = 10   if sample_size==1 else 3
-				lines.append(f'''sbatch --export=ALL,experiment="{exp}",func_name="{func}",method="noNN",frequency="{freq}",frequency_save="{freq}",diversity_method={diversity_method},scale_factor="Random",save="True",pbar="False",silent="False",cluster="True" cluster_job.sh\n''')
+				lines.append(f'''sbatch --export=ALL,experiment="{exp}",func_name="{func}",method="noNN",frequency="{freq}",frequency_save="{freq}",diversity_method={diversity_method},scale_factor="Random",save="True",pbar="False",silent="False",cluster="True" cluster_job_gpu.sh\n''')
 
 with open(out_file, 'w') as f: f.writelines(lines)
 
