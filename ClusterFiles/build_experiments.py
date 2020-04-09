fa2a170e04b10ce6f4511de1be6ee734bcb66a6c
Maryam Hasani Shoreh <a1711759@l03.phoenix.adelaide.edu.au>
2020-04-09 18:00:30 +0930

    exps

--- ClusterFiles/build_experiments.py
+++ ClusterFiles/build_experiments.py
@@ -12,13 +12,13 @@ lines = []
 # sbatch --export=ALL,experiment="exp1",func_name="sphere",method="noNN",frequency=1,frequency_save=1,diversity_method=None,save="True",pbar="False",silent="True",cluster="True"
 # for replace_mech in ['Random','Worst']:
 	# for sample_size in [1,3,7,9]: noNNRestart
-for freq in [0.5,1,4,10,20]:	
-	for diversity_method in ['RI', 'None', 'CwN', 'HMu','Rst']:#'RI', 'None', 'CwN', 'HMu'
+for freq in [10]:	
+	for diversity_method in ['RI', 'None', 'HMu','Rst']:#'RI', 'None', 'CwN', 'HMu'
 		for exp in ['exp1','exp2','exp3','exp4']:
 			for func in ['sphere','rosenbrock','rastrigin']:
 				# train_window = None if sample_size==1 else 5
 				# epochs       = 10   if sample_size==1 else 3
-				lines.append(f'''sbatch --export=ALL,experiment="{exp}",func_name="{func}",method="noNN",frequency="{freq}",frequency_save="{freq}",diversity_method={diversity_method},scale_factor="Random",save="True",pbar="False",silent="False",cluster="True" cluster_job_gpu.sh\n''')
+				lines.append(f'''sbatch --export=ALL,experiment="{exp}",func_name="{func}",method="noNN",frequency="{freq}",frequency_save="{freq}",diversity_method={diversity_method},scale_factor="Random",save="True",pbar="False",silent="False",cluster="True" cluster_job.sh\n''')
 
 with open(out_file, 'w') as f: f.writelines(lines)
 
