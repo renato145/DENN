a34a31854d660655062fe413b3b4e6f3bdd70645
Maryam Hasani Shoreh <a1711759@l03.phoenix.adelaide.edu.au>
2020-04-11 11:44:08 +0930

    exps

--- ClusterFiles/build_experiments.py
+++ ClusterFiles/build_experiments.py
@@ -12,7 +12,7 @@ lines = []
 # sbatch --export=ALL,experiment="exp1",func_name="sphere",method="noNN",frequency=1,frequency_save=1,diversity_method=None,save="True",pbar="False",silent="True",cluster="True"
 # for replace_mech in ['Random','Worst']:
 	# for sample_size in [1,3,7,9]: noNNRestart
-for freq in [30]:	
+for freq in [20]:	
 	for diversity_method in ['RI', 'None', 'HMu','Rst']:#'RI', 'None', 'CwN', 'HMu'
 		for exp in ['exp1','exp2','exp3','exp4']:
 			for func in ['sphere','rosenbrock','rastrigin']:
